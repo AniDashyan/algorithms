4839c3f87644a6ddfa2afdb3b28f100f0881db1d
AniDashyan <ani.dashyan77@gmail.com>
2025-03-12 19:32:21 +0400

    Implement find, find_if, find_if_not, Add tests

--- algorithms.hpp
+++ algorithms.hpp
@@ -1,10 +1,57 @@
 #pragma once
 
-template< class InputIt, class UnaryFunc >
-UnaryFunc for_each( InputIt first, InputIt last, UnaryFunc f ) {
-    while (first != last) {
-        f(*first);
-        first++;
+#include  <utility>
+
+namespace ani {
+    template< class InputIt, class UnaryFunc >
+    constexpr UnaryFunc for_each( InputIt first, InputIt last, UnaryFunc f ) {
+        while (first != last) {
+            f(*first);
+            ++first;
+        }
+        return (f);
     }
-    return (f);
+
+    template< class InputIt, class Size, class UnaryFunc >
+    constexpr std::pair<InputIt, UnaryFunc> for_each_n( InputIt first, Size n, UnaryFunc f ) {
+        for (; n > 0; --n, ++first) {
+            f(*first);
+        }
+        return {first, f};       
+    }
+
+    template< class InputIt, class T >
+    constexpr std::pair<InputIt, bool> find( InputIt first, InputIt last, const T& value ) {
+        while (first != last)
+        {
+            if (*first == value)
+                return {first, true};
+
+            ++first;
+        }
+        return {last, false};
+    } 
+
+    template< class InputIt, class UnaryPred >
+    constexpr std::pair<InputIt, bool> find_if( InputIt first, InputIt last, UnaryPred p ) {
+        while (first != last)
+        {
+            if (p(*first)) {
+                return {first, true};
+            }
+            ++first;
+        }
+        return {last, false};
+    }
+
+    template< class InputIt, class UnaryPred >
+    constexpr std::pair<InputIt, bool> find_if_not( InputIt first, InputIt last, UnaryPred q ) {
+        while (first != last) {
+            if (!q(*first)) {
+                return {first, true};
+            }
+            ++first;
+        }
+        return {last, false};
+    }
 }
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 void add_and_print(int x) {
     std::cout << "x: " << x + 7 << "\n";
@@ -21,28 +22,48 @@ class Add {
         int get_sum() const {
             return (this->m_sum);
         } 
-}; 
+};
+
+
+class OddCount {
+    private:
+        int m_count;
+    public:
+        OddCount() : m_count(0) {}
+        bool operator()(int x) {
+            if (x % 2 == 1)
+                ++m_count;
+
+            return (true);
+        }
+    int get_count() const {
+        return (this->m_count);
+    }
+};
 
 int main() {
     std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7};
     std::vector<std::string> text = {"Hello", "darkness", "my", "old", "friend"};
+    std::vector<char> chars{'H', 'd', 'm', 'o', 'f'};
 
+    std::cout << "Testing for_each\n";
     // Calling with a regular function
     std::cout << "Test 1: for_each with regular function\n";
+    ani::for_each(vec.begin(), vec.end(), add_and_print);
     for_each(vec.begin(), vec.end(), add_and_print);
     
     std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
 
     // Calling with lambda function
     std::cout << "Test 2: for_each with lambda functions\n";
-    for_each(vec.begin() + 1, vec.end() - 1, [](int& x) {
+    ani::for_each(vec.begin() + 1, vec.end() - 1, [](int& x) {
         std::cout << x + 7 << "\n";
     });
 
     std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
 
     std::cout << "Test 2.1: for_each with lambda functions on string\n";
-    for_each(text.begin(), text.end(), [](const std::string& i) {
+    ani::for_each(text.begin(), text.end(), [](const std::string& i) {
         std::cout << "length of " << i << " is " << i.length() << "\n"; 
     });
 
@@ -50,8 +71,66 @@ int main() {
     
     // for_each returns the function object state
     std::cout << "Test 3: Calling for_each with functor (function object)\n";
-    int s = for_each(vec.begin(), vec.end(), Add()).get_sum();
+    int s = ani::for_each(vec.begin(), vec.end(), Add()).get_sum();
     std::cout << "sum: " << s << "\n";
-  
+    
+    std::cout << std::setfill(' ') << std::setw(30) << "" << std::setfill(' ') << "\n";
+    std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
+
+    std::cout << "Testing for_each_n\n";
+    size_t n = 4;
+    int k = 100;
+    std::cout << "Testing with a regular function\n";
+    // std::vector<int>::iterator it = vec.begin();
+    auto [x, y]= ani::for_each_n(vec.begin(), n, add_and_print);
+    std::cout << *x << "----" << y << "\n";
+
+    std::cout << std::setfill(' ') << std::setw(30) << "" << std::setfill(' ') << "\n";
+    std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
+    
+    std::cout << "Testing find\n";
+    std::cout << "ani::find\n";
+    auto [it, f] = ani::find(vec.begin(), vec.end(), 7);
+    if (f) std::cout << "Found 7 at " << *it << "\n";
+    else std::cout << "Couldn't find 7\n";
+    
+    // std::find
+    std::cout << "std::find\n";
+    std::vector<int>::iterator it1 = std::find(vec.begin(), vec.end() - 3, 1);
+    if (it1 != vec.end()) std::cout << "Found 1 at " << *it1 << "\n";
+    else std::cout << "Couldn't find 1\n"; 
+    
+    std::cout << std::setfill(' ') << std::setw(30) << "" << std::setfill(' ') << "\n";
+    std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
+
+    auto [it11, f1] = ani::find(vec.begin(), vec.end() - 3, 1);
+    if (f) std::cout << "Found 1 at " << *it1 << "\n";
+    else "Couldn't find 1\n";
+    
+    std::cout << std::setfill(' ') << std::setw(30) << "" << std::setfill(' ') << "\n";
+    std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
+
+    std::cout << "Testing find_if\n";
+
+    auto [it_if, f_if] = ani::find_if(vec.begin(), vec.end(), OddCount());
+    auto [it_iff, f_iff] = ani::find_if(vec.begin(), vec.end(), OddCount());
+    if (f_if) {
+        std::cout << "Found at " << *it_if << "\n";
+        std::cout << "Found at : " << std::distance(vec.begin(), it_if) << "\n";
+    }
+    else  std::cout << "Couldn't find\n";
+
+    std::cout << std::setfill(' ') << std::setw(30) << "" << std::setfill(' ') << "\n";
+    std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
+
+    std::cout << "Total Odd count " << OddCount().get_count() << "\n";
+
+    std::cout << "Testing find_if_not\n";
+    auto [itt, ff] = ani::find_if_not(chars.begin(), chars.end(), isdigit);
+    if (ff) std::cout << "The first not digit " << *itt << "\n";
+    else std::cout << "Vector contains only digits\n";
+
+    std::cout << std::setfill(' ') << std::setw(30) << "" << std::setfill(' ') << "\n";
+    std::cout << std::setfill('-') << std::setw(30) << "" << std::setfill(' ') << "\n";
     return (0);
 }
